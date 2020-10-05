19d5ed76a3ec0998fa6df5a2b90c0ba011bed967
KGSSV <kgssvak@gmail.com>
2020-10-05 08:50:59 +0530

    updated

--- Tkinter_Basic/buttons_and_grid.py
+++ Tkinter_Basic/buttons_and_grid.py
@@ -21,7 +21,7 @@ but2 = tkinter.Button(root, text='butt2', bg="#ff0000")
 but2.grid(row='0', column='1', padx=(10, 0))
 
 # activebackground changes collor to the hex code when interacted
-but3 = tkinter.Button(root, text='butt3', bg="#ff0000",
+but3 = tkinter.Button(root, text='butt3butt3', bg="#ff0000",
                       activebackground="#0000ff")
 but3.grid(row='0', column='2', padx=(10, 0))
 
@@ -30,7 +30,23 @@ but4 = tkinter.Button(root, text='butt2', bg="#ff0000",
                       activebackground='lightblue')
 but4.grid(row='1', column='0', columnspan=3, sticky='WE', pady=(10, 0))
 
-# always remember (grid is only fr placement and using padding etc check termical for erroe)
+# but we have one complication the width for the column is determined by the largest button of that collumn and
+# the following addtions are centered
+# lets see this
+
+but5 = tkinter.Button(root, text='but', bg="#ff0000",
+                      )
+but5.grid(row='2', column='0', padx=(10, 0))
+
+but6 = tkinter.Button(root, text='but', bg="#ff0000",
+                      )
+but6.grid(row='2', column='1', padx=(10, 0))
+
+but7 = tkinter.Button(root, text='but', bg="#ff0000",
+                      )
+but7.grid(row='2', column='2', padx=(10, 0), sticky='w')
+
+# always remember (grid is only fr placement and using padding etc check terminal for erroe)
 
 # for grid use the following prop ( row , colum , padx pady , ipadx ipad y , columnspan , rowspan)
 root.mainloop()
